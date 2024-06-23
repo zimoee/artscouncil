93c2326d126f524527d9468c9acc744b905298e7
Zimo Huang <111151640+zimoee@users.noreply.github.com>
2024-06-23 17:23:25 -0400

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+						   
+	$(".countdown").countdown({
+				date: "02 November 2023 11:20:00",
+				format: "on"
+			},
+			
+			function() {
+				// callback function
+			});
+
+});	
